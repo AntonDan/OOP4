456f94e2192b83d102e7478d56aa69fa756cdf5f
AntonDan <anton.dan@hotmail.com>
2015-02-27 03:09:21 +0200

    sfd

---
+++ Askisi4/Askisi4/Interface.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include <cstdio>
+#include <fstream>
+#include <vector>
+#include <cstdarg>
+#include "DS.h"
+#include "Users.h"
+
+using namespace std;
+
+void MainMenu(User * user) {
+	cout << "ÅðéëÝîôå: \n "
+		<< "ôï ID ôïõ öüñïõì ðïõ åðéèõìåßôå íá åðéóêåöèåßôå \n"
+		<< "L, ãéá óþóéìï ôçò ôñÝ÷ïõóáò êáôÜóôáóçò êáé Ýîïäï áðü ôï óýóôçìá"
+		<< "X, ãéá Ýîïäï áðü ôï óýóôçìá ÷ùñßò íá óùèåß ç ôñÝ÷ïõóá êáôÜóôáóÞ ôïõ" << endl;
+
+	if (user->GetRights() == 3) {
+		cout << "N, ãéá äçìéïõñãßá íÝïõ öüñïõì \n"
+			<< "U, ãéá äéá÷åßñéóç ÷ñçóôþí" << endl;
+	}
+
+}
