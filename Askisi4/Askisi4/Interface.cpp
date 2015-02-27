6fb06d1d5f1adb7c8f197821b250eba023ecb8e6
AntonDan <anton.dan@hotmail.com>
2015-02-27 05:15:44 +0200

    commit

--- Askisi4/Askisi4/Interface.cpp
+++ Askisi4/Askisi4/Interface.cpp
@@ -3,25 +3,43 @@
 #include <fstream>
 #include <vector>
 #include <cstdarg>
-#include "DS.h"
-#include "Users.h"
+#include "ForumManager.h"
 
 using namespace std;
 
-void MainMenu(User * user) {
+void MainMenu(ForumManager & man,  User * user) {
 	cout << "Select: \n "
 		<< "The ID of the Forum, you wish to visit. \n"
 		<< "L, to Save the current system state and Exit. \n"
 		<< "X, to Exit without Saving. " << endl;
 
 	if (user->GetRights() == 3) {
-		cout << "N, to create a new forum \n"
-			<< "U, to Manage the User Database. " << endl;
+		cout << "N, to Create a new Forum \n"
+	   		<< "U, to Manage the User Database. " << endl;
+	}
+
+	char selection;
+	cin.clear();
+	cin.sync();
+	cin >> selection;
+
+	switch (selection) {
+	case 'L':
+		break;
+	case  'X':
+		break;
+	case 'N':
+		if (user->GetRights() < 3) break;
+		cout << "Enter forum name: ";
+		string name;
+		cin >> name;
+		man.CreateForum(man.GetMain(), name);
+		break;
 	}
 
 }
 
-void UserMenu(User * user){
+void UserMenu(User * user) {
 	cout << "Select: \n"
 		<< "C, to view Users Catalogue. \n"
 		<< "M, to Change Rights. \n"
@@ -38,14 +56,14 @@ void ForumMenu(User * user){
 		<< "T, to Visit Thread menu. \n"
 		<< "B, to go Back. \n"
 		<< "C, to Create Thread. \n"
-		<< "H, to return to Main Menu. \n"
+		<< "H, to Return to Main Menu. \n"
 		<< "L, to Save the current system state and Exit. \n"
 		<< "X, to Exit without Saving. " << endl;
 
 	if (user->GetRights() == 3){
 		cout << "N, to Create Forum. \n"
 			<< "E, to Rename Forum. \n"
-			<< "D, to Felete Forum. \n"
+			<< "D, to Delete Forum. \n"
 			<< "M, to Move Forum. " << endl;
 	}
 }
@@ -54,7 +72,7 @@ void ThreadMenu(User * user){
 	cout << "Select: \n"
 		<< "B, to go Back. \n"
 		<< "R, to Reply to a Post. \n"
-		<< "H, to return to Main Menu. \n"
+		<< "H, to Return to Main Menu. \n"
 		<< "L, to Save the current system state and Exit. \n"
 		<< "X, to Exit without Saving." << endl;
 
